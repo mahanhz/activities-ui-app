6c8c704aad28a4849c1a37b2d620ba5b90963e0a
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-10-08 12:36:26 +0200

    Initial commit

---
+++ src/main/java/com/amhzing/activities/ui/ActivitiesUiApplication.java
@@ -0,0 +1,12 @@
+package com.amhzing.activities.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesUiApplication.class, args);
+	}
+}
