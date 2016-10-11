7a193046e12762227e8ad6f67ecb8d181ff8b845
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-10-11 15:14:42 +0200

    Custom success handler logic uses either saved url or role based url

--- src/main/java/com/amhzing/activities/ui/configuration/handler/RedirectAuthenticationSuccessHandler.java
+++ src/main/java/com/amhzing/activities/ui/configuration/handler/RedirectAuthenticationSuccessHandler.java
@@ -2,9 +2,11 @@ package com.amhzing.activities.ui.configuration.handler;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,18 +14,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-import static com.amhzing.activities.ui.configuration.AppSecurityConfig.VAADIN_USER;
+import static com.amhzing.activities.ui.configuration.AuthenticationConfig.ADMIN;
+import static com.amhzing.activities.ui.configuration.AuthenticationConfig.VAADIN_USER;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.replace;
 
-public class RedirectAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class RedirectAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private static final String ROLE = "ROLE_";
+    private RequestCache requestCache = new HttpSessionRequestCache();
 
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
-        // request.getRequestDispatcher(determineForwardUrl(authentication)).forward(request, response);
-        final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-        redirectStrategy.sendRedirect(request, response, url(authentication));
+        final SavedRequest savedRequest = requestCache.getRequest(request, response);
+        String targetUrl = "";
+
+        final String roleBasedTargetUrl = url(authentication);
+
+        if ((savedRequest == null) && isBlank(roleBasedTargetUrl)) {
+            super.onAuthenticationSuccess(request, response, authentication);
+            return;
+        }
+
+        clearAuthenticationAttributes(request);
+
+        targetUrl = useSavedUrl(savedRequest, request) ? savedRequest.getRedirectUrl() : roleBasedTargetUrl;
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
     String url(Authentication authentication) {
@@ -34,8 +51,25 @@ public class RedirectAuthenticationSuccessHandler implements AuthenticationSucce
 
         if (roles.contains(ROLE + VAADIN_USER)) {
             return "/vaadin";
-        } else {
-            throw new IllegalStateException();
+        } else if (roles.contains(ROLE + ADMIN)) {
+            return "/togglz-console";
         }
+
+        return null;
+    }
+
+    private boolean useSavedUrl(final SavedRequest savedRequest, final HttpServletRequest request) {
+        if (savedRequest == null) {
+            return false;
+        } else if (savedRequest instanceof DefaultSavedRequest) {
+            final String strippedSavedRequestURI = replace(((DefaultSavedRequest) savedRequest).getRequestURI(), "/", "");
+            final String strippedRequestContextPath = replace(request.getContextPath(), "/", "");
+
+            if (strippedSavedRequestURI.equalsIgnoreCase(strippedRequestContextPath)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
