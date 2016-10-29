744041fab811fae2043d0ea7df868d4425704059
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-10-29 17:57:07 +0200

    methods did not need to be beans

--- src/main/java/com/amhzing/activities/ui/configuration/AuthenticationConfigOnline.java
+++ src/main/java/com/amhzing/activities/ui/configuration/AuthenticationConfigOnline.java
@@ -3,7 +3,6 @@ package com.amhzing.activities.ui.configuration;
 import com.amhzing.activities.ui.annotation.Online;
 import com.amhzing.activities.ui.user.DefaultUserDetailsService;
 import com.amhzing.activities.ui.user.UserAuthenticationProvider;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,18 +13,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @Configuration
 public class AuthenticationConfigOnline extends GlobalAuthenticationConfigurerAdapter {
 
-    @Bean
-    private UserDetailsService userDetailsService() {
-        return new DefaultUserDetailsService();
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
     }
 
-    @Bean
     private AuthenticationProvider authenticationProvider() {
         return new UserAuthenticationProvider(userDetailsService());
     }
 
-    @Override
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    private UserDetailsService userDetailsService() {
+        return new DefaultUserDetailsService();
     }
 }
