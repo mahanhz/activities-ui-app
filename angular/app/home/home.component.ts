e84e10cfe2213d85ab81eb75e2062d01e58856a4
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-11-08 20:46:32 +0100

    Moved access service to its own folder

--- angular/app/home/home.component.ts
+++ angular/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AccessService } from './access.service';
+import { AccessService } from 'app/access/access.service';
 
 interface Access {
     includeFacilitatedActivity: boolean;
